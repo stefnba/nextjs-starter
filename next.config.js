7227f2e3590c18d9a0331a6941db84833df39a7c
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-11-25 17:24:50 +0100

    fix: image remote pattern update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com']
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                pathname: '**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                pathname: '**'
+            }
+        ]
     }
 };
 

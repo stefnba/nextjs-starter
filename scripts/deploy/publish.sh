e51e136183bee83944703baee2935ae8bb64ae1d
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-11-26 15:45:42 +0100

    fix: deploy actions

--- scripts/deploy/publish.sh
+++ scripts/deploy/publish.sh
@@ -15,9 +15,7 @@ docker compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml -f
 docker compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml -f docker/docker-compose.deploy.yml --project-directory . pull
 
 # migrate db
-docker compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml -f docker/docker-compose.deploy.yml --project-directory . run --rm -it --entrypoint "npm run migrate up" app
+# docker compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml -f docker/docker-compose.deploy.yml --project-directory . run --rm -it --entrypoint "npm run migrate up" app
 
 # restart containers
 docker compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml -f docker/docker-compose.deploy.yml --project-directory . up -d --build --remove-orphans
-
-docker ps
